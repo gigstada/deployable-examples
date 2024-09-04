26ac2ccf369e8f362da619f47ffb34ac0067c2c5
Adam Gigstad <adam.gigstad@gmail.com>
2024-09-03 21:15:31 -0700

    Remove auth

--- fastapi_gcp/server.py
+++ fastapi_gcp/server.py
@@ -9,37 +9,9 @@ import os
 
 load_dotenv()
 
-# Main FastAPI app
+
+# Main FastAPI app with global dependency for authentication
 app = FastAPI()
-
-# Define security
-security = HTTPBasic()
-
-# Get credentials from environment variables
-LOGIN_USERNAME = os.getenv("LOGIN_USERNAME")
-LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
-
-
-# Function to validate username and password
-def get_current_username(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
-):
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = LOGIN_USERNAME.encode("utf8")
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
-    current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = LOGIN_PASSWORD.encode("utf8")
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
-    if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
 
 
 # API route group
@@ -50,7 +22,6 @@ api_app = FastAPI()
 def read_item(
     item_id: int,
     q: Union[str, None] = None,
-    credentials: Annotated[HTTPBasicCredentials, Depends(get_current_username)] = None,
 ):
     return {"item_id": item_id, "q": q}
 
